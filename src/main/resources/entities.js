f5e009786f5c27c4d501a4772aeffb4e51007c31
gary <gary@garysieling.com>
2017-12-14 07:56:02 -0500

    fix error

--- src/main/resources/entities.js
+++ src/main/resources/entities.js
@@ -2,24 +2,28 @@ let fs = require('fs');
 
 let entities = fs.readFileSync('d:\\projects\\scala-indexer\\entities.txt');
 let dir = 'c:\\projects\\image-annotation\\data\\talks\\json\\1';
+let _ = require('lodash');
 
 (entities + '').split("\n").map(
   (line) => {
     let parts = line.split(",");
-    let token = parts[0];
-    let id = parts[1].trim();
+    if (parts.length == 2) {
+      let token = parts[0];
+      let id = parts[1].trim();
 
-    let filename = dir + "\\" + id + ".json";
-    console.log(filename)
+      let filename = dir + "\\" + id + ".json";
+      console.log(filename)
 
-    let data = JSON.parse(fs.readFileSync(filename, 'utf-8'));
+      let data = JSON.parse(fs.readFileSync(filename, 'utf-8'));
 
-    if (!data.entities) {
-      data.entities = [];
+      if (!data.entities_ss) {
+        data.entities_ss = [];
+      }
+
+      data.entities_ss.push(token);
+      data.entities_ss = _.uniq(data.entities_ss);
+
+      fs.writeFileSync(filename, JSON.stringify(data, null, 2));
     }
-
-    data.entities.push(token);
-
-    fs.writeFileSync(filename, JSON.stringify(data));
   }
 )
